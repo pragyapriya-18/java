2d7e6ed645bf5a069c75a5d1dad3acc94234e79e
pragyapriya-18 <pragyapriya.530@gmail.com>
2025-04-26 21:39:00 +0530

    Added

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,20 @@
+class PrimitiveDatatypes{
+    public static void main(String[] args) {
+        char a = 'a';
+        int i = 89;
+        float f = 45.2f;
+        byte b = 4;
+        short s = 56;
+        double d = 45.5d;
+        boolean c = true;
+
+        System.out.println("char: " + a);
+        System.out.println("int: " + i);
+        System.out.println("float: " + f);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("double: " + d);
+        System.out.println("boolean: " + c);
+
+    }
+}
