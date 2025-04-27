33b24ad0124b3b68edd5374c7280f1f8f9154343
pragyapriya-18 <pragyapriya.530@gmail.com>
2025-04-27 20:32:40 +0530

    Added

--- for statement.java
+++ for statement.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
 class ForLoopSum{
     public static void main(String[]args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter a number:");
-        int n=sc.nextInt();
         int sum=0;
 
         // Using for loop to calculate sum
-        for(int i=1;i<=n;i++){
+        for(int i=1; i<=10; i++){
             sum=sum +i;
         }
-        System.out.println("sum of first:"+n+"natural no is"+sum);
-        sc.close();
-        
+         System.out.println("sum: " + sum); 
     }
 }
