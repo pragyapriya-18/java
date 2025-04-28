925a655d5e5cfed8cc6eb092d9ae43a95f3da5f6
Pragya priya srivastava <pragyapriya.530@gmail.com>
2025-04-28 08:32:11 +0530

    Added

--- StrBufferMethod.java
+++ StrBufferMethod.java
@@ -3,7 +3,13 @@ class StringBufferDemo {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("Java");
 
-        sb.append(" Programming");
+        sb.append("Programming");
+        System.out.println(sb);
+
+        sb.insert("Language ");
+        System.out.println(sb);
+
+        sb.reverse ();
         System.out.println(sb);
     }
 }
