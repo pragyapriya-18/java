31608d244f561cd19f3b350895bc5b3e5285ad7d
Pragya priya srivastava <pragyapriya.530@gmail.com>
2025-04-28 07:55:23 +0530

    Added

--- while statement.java
+++ while statement.java
@@ -18,3 +18,17 @@ class WhileLoopReverse {
         sc.close();
     }
 }
+
+---------------------------
+
+// Print numbers from 1 to 5 using while loop in Java
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
